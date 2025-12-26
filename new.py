4599cc08e1183e91d8ae2a58a2e1f1a53e2dedbb
unknown <salikesuya@gmail.com>
2025-12-26 13:25:24 +0545

    first commit

---
+++ new.py
@@ -0,0 +1,15 @@
+School_name="KENDRIYA VIDHALAYA"
+Student_name="Suya Salike"
+Student_class="VII"
+Student_section="A"
+Gaurdian_name="Suman Salike"
+Contact_no_1=9841923660
+Contact_no_2=9841153299
+Principal_name="A.Jerald"
+print(School_name)
+print("Student's name",Student_name)
+print("Class",Student_class)
+print("Section",Student_section)
+print("Gaurdian",Gaurdian_name)
+print("Coonntact.No",Contact_no_1,Contact_no_2)
+print("Principal's name",Principal_name)
