4e82044342f4cef16418741dfd7eebf315972c07
salikesuya-rgb <salikesuya@gmail.com>
2025-12-26 13:58:07 +0545

    MarkSheet added and rename new.py to IDCard.py

---
+++ MarkSheet.py
@@ -0,0 +1,18 @@
+English=58
+Hindi=59
+Sanskrit=45
+Social_Science=59
+Science=60
+Math=57
+print("KENDRIYA VIDHYALAYA")
+print("MARKSHEET")
+print("Students name:Suya Salike")
+print("Class:VII")
+print("Section:A")
+print("Roll.No")
+print("ENGLISH",English)
+print("HINDI",Hindi)
+print("SANSKRIT",Sanskrit)
+print("SOCIAL SCIENCE",Social_Science)
+print("SCIENCE",Science)
+print("MATHEMATICS",Math)
