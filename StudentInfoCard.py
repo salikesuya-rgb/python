421a095b645dbcc2b318b803af98dec712747417
salikesuya-rgb <salikesuya@gmail.com>
2025-12-26 14:18:04 +0545

    added a Student Info Card

---
+++ StudentInfoCard.py
@@ -0,0 +1,16 @@
+Student_Name="Suya Salike"
+Student_Class="VII"
+Student_Section="A"
+Blood_Group="O+"
+Student_Age=12
+DOB="10th January 2013"
+Student_Contact_No_1=9841923660
+Student_Contact_No_2=9841153299
+print("STUDENT-INFO")
+print("Student's Name:",Student_Name)
+print("Class:",Student_Class)
+print("Section:",Student_Section)
+print("Blood Group:",Blood_Group)
+print("Age:",Student_Age)
+print("DOB:",DOB)
+print("Contact.No:",Student_Contact_No_1,Student_Contact_No_2)
